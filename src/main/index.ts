c7b6e0e454a935b1d571e3d88f9fe5ffe6d89960
yusrilaprial <myusrila23@gmail.com>
2025-02-03 10:51:57 +0700

    feat: add tzuchi and puppeteer

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,6 +2,7 @@ import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
+import puppeteer, { Browser, Page } from 'puppeteer'
 
 function createWindow(): void {
   // Create the browser window.
@@ -50,7 +51,97 @@ app.whenReady().then(() => {
   })
 
   // IPC test
-  ipcMain.on('ping', () => console.log('pong'))
+  let count = 0
+  ipcMain.on('ping', (event) => {
+    const message = 'Pong' + count
+    console.log(message)
+    event.reply('ping-reply', message)
+    count++
+  })
+
+  // IPC Test for Tzuchi
+  const URL = 'https://tzuchipalembang.org'
+  const USERNAME = 'sa'
+  const PASSWORD = 'sa'
+  let tzuchiBrowser: Browser | null = null
+  let tzuchiFirstPage: Page | null = null
+  const launchTzuchiBrowser = async () => {
+    if (!tzuchiBrowser) {
+      tzuchiBrowser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: null,
+        userDataDir: './user-data'
+      })
+    }
+    if (!tzuchiFirstPage) tzuchiFirstPage = await tzuchiBrowser.pages().then((pages) => pages[0])
+  }
+  const closeTzuchiBrowser = async () => {
+    if (tzuchiFirstPage) {
+      await tzuchiFirstPage.close()
+      tzuchiFirstPage = null
+    }
+    if (tzuchiBrowser) {
+      await tzuchiBrowser.close()
+      tzuchiBrowser = null
+    }
+  }
+  const resetTzuchiBrowser = async () => {
+    tzuchiBrowser = null
+    tzuchiFirstPage = null
+    await launchTzuchiBrowser()
+  }
+  ipcMain.handle('login-tzuchi', async () => {
+    try {
+      await launchTzuchiBrowser()
+
+      await tzuchiFirstPage!.goto(URL + '/login')
+      if (tzuchiFirstPage!.url() !== URL + '/login') {
+        console.log('Already Login Tzuchi')
+        return
+      }
+
+      const selectorUsername = `div.ui.stacked.segment > div.field > input[type="text"][placeholder="VIS ID / Email / Phone"]`
+      const selectorPassword = `div.ui.stacked.segment > div.field > div.ui.icon.input > input[type="password"][placeholder="Password"]`
+      const selectorSubmit = `div.ui.stacked.segment > button.ui.fluid.large.blue.submit.button`
+      await tzuchiFirstPage!.locator(selectorUsername).fill(USERNAME)
+      await tzuchiFirstPage!.locator(selectorPassword).fill(PASSWORD)
+      await tzuchiFirstPage!.locator(selectorSubmit).click()
+      await tzuchiFirstPage!.waitForNavigation()
+      console.log('Login Tzuchi')
+    } catch (error) {
+      await resetTzuchiBrowser()
+      throw error
+    }
+  })
+
+  ipcMain.handle('logout-tzuchi', async () => {
+    try {
+      if (!tzuchiBrowser || !tzuchiFirstPage) {
+        console.log('Please Login Tzuchi First')
+        return
+      }
+
+      await tzuchiFirstPage!.goto(URL + '/superadmin')
+      if (tzuchiFirstPage!.url() !== URL + '/superadmin') {
+        console.log('Already Logout Tzuchi')
+        return
+      }
+
+      const selectorDropdown = `div.right.menu > div.ui.dropdown.item`
+      const selectorLogout = `div.right.menu > div.ui.dropdown.item.active.visible > div.menu.left.transition.visible > a:nth-child(3)`
+      await tzuchiFirstPage!.locator(selectorDropdown).click()
+      await tzuchiFirstPage!.locator(selectorLogout).click()
+      await tzuchiFirstPage!.waitForNavigation()
+
+      setTimeout(async () => {
+        await closeTzuchiBrowser()
+        console.log('Logout Tzuchi')
+      }, 5000)
+    } catch (error) {
+      await resetTzuchiBrowser()
+      throw error
+    }
+  })
 
   createWindow()
 
--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import Versions from './components/Versions.vue'
 
+const message = ref('')
+
 const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+window.electron.ipcRenderer.on('ping-reply', (_, data) => {
+  message.value = data
+})
+
+const loginTzuchi = () => window.electron.ipcRenderer.invoke('login-tzuchi')
+const logoutTzuchi = () => window.electron.ipcRenderer.invoke('logout-tzuchi')
 </script>
 
 <template>
@@ -14,12 +23,19 @@ const ipcHandle = () => window.electron.ipcRenderer.send('ping')
     <span class="ts">TypeScript</span>
   </div>
   <p class="tip">Please try pressing <code>F12</code> to open the devTool</p>
+  <p class="tip">{{ message }}</p>
   <div class="actions">
     <div class="action">
       <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">Documentation</a>
     </div>
     <div class="action">
       <a target="_blank" rel="noreferrer" @click="ipcHandle">Send IPC</a>
+    </div>
+    <div class="action">
+      <a target="_blank" rel="noreferrer" @click="loginTzuchi">Login Tzuchi</a>
+    </div>
+    <div class="action">
+      <a target="_blank" rel="noreferrer" @click="logoutTzuchi">Logout Tzuchi</a>
     </div>
   </div>
   <Versions />
